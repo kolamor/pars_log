00af12036c79fb859590a799e0d7f446a6f627fc
kolamor <kolamorev@mail.ru>
2019-04-18 23:53:12 +0300

    fix

--- parslog.py
+++ parslog.py
@@ -6,36 +6,38 @@ FILENAME = r'maillog'
 
 class ParserLog():
 
-	re_from_send_email = r'''\bfrom=<(.+)>.+size=.+nrcpt='''
-	re_to_send_email = r'''\bto=<(.+)>.+relay=.+dsn=.+status='''
-	re_success_send = r'''\bto=<(.+)>.+relay=.+dsn=.+status=sent'''
-	re_falled_send= r'''\bto=<(.+)>.+relay=.+dsn=.+status=(?!sent)'''
+	re_from_send_email = r'''\bfrom=(.+),.+size=.+nrcpt='''
+	re_success_send = r''': (.+): to=.+,.+relay=.+dsn=.+status=sent'''
+	re_falled = r''': (.+):.+to=.+,.+relay=.+dsn=.+status=(?!sent)'''
+	re_removed = r''': (.+): removed'''
 
 	def __init__(self, log):
 		self.log = log
 
 	def send_from(self):
-		'''С какого ящика и сколько писем было отправлено'''
+		'''Количество писем с почтового ящика'''
 		mail_list = re.findall(self.re_from_send_email, self.log)
 		count = Counter(mail_list)
 		return dict(count)
 
-	def count_send_to(self):
-		'''Общее кол-во отправлений'''
-		mail_list =re.findall(self.re_to_send_email, self.log)
-		return len(mail_list)
-
 	def count_success_send(self):
 		'''Кол-во удачных отправлений'''
 		mail_list = re.findall(self.re_success_send, self.log)
-		# count = Counter(mail_list) # кто и сколько
-		return len(mail_list) #, dict(count)
+		return len(mail_list)
 
 	def count_falled_send(self):
 		'''Кол-во проваленных'''
-		mail_list = re.findall(self.re_falled_send, self.log)
-		# count = Counter(mail_list) # кто и сколько
-		return len(mail_list) #, dict(count)
+		list_falled = re.findall(self.re_falled, self.log)
+		removed_list = re.findall(self.re_removed, self.log)
+		removed_falled =[]
+		for id_notsent in list_falled:
+			try:
+				index = removed_list.index(id_notsent)
+			except ValueError:
+				continue
+			removed_falled.append(removed_list.pop(index))
+		return len(removed_falled)
+
 
 def reader(file):
 
@@ -47,10 +49,10 @@ def reader(file):
 def main():
 	log = reader(FILENAME)
 	resend = ParserLog(log)
-	print( resend.send_from() )
-	print('all sent :', resend.count_send_to() )
+	print('from', resend.send_from() )
 	print('success  :', resend.count_success_send() )
 	print('falled   :', resend.count_falled_send())
+
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from anguro_parslog import ParserLog
+from parslog import ParserLog
 
 
 
@@ -10,7 +10,7 @@ class TestLog(unittest.TestCase):
 	def test_send_from_1(self):
 		log = '''Jul 10 10:09:53 srv24-s-st postfix/qmgr[3043]: 5FEB2DF0506: from=<it@yc4.ru>, size=788, nrcpt=1 (queue active)'''
 		pars = ParserLog(log)
-		self.assertEqual(pars.send_from(), {'it@yc4.ru' : 1})
+		self.assertEqual(pars.send_from(), {'<it@yc4.ru>' : 1})
 
 	def test_send_from_2(self):
 		log = ''
@@ -25,11 +25,11 @@ class TestLog(unittest.TestCase):
 	def test_send_from_4(self):
 		log = '''ul 10 10:09:53 srv24-s-st postfix/qmgr[3043]: 5FEB2DF0506: from=<it@yc4.ru>, size=788, nrcpt=1 (queue active)
 		Jul 10 10:09:53 srv24-s-st postfix/qmgr[3043]: 5FEB2DF0506: from=<it@yc.ru>, size=788, nrcpt=1 (queue active)
-		Jul 10 10:09:52 srv24-s-st postfix/qmgr[3044]: 5FEB2DF0506: from=<it@yc.ru>, size=788, nrcpt=1 (queue active)
+		Jul 10 10:09:52 srv24-s-st postfix/qmgr[3044]: 5FEB2DF0507: from=<it@yc.ru>, size=788, nrcpt=1 (queue active)
 		Jul 10 10:09:22 srv24-s-st postfix/smtp[22635]: D69F5DF04F4: to=<krasteplokomplekt@yandex.ru>,'''
 		pars = ParserLog(log)
-		self.assertEqual(pars.send_from(), {'it@yc4.ru' : 1,
-											'it@yc.ru'  : 2,
+		self.assertEqual(pars.send_from(), {'<it@yc4.ru>' : 1,
+									'<it@yc.ru>'  : 2,
 											})
 
 class TestLogSetUp(unittest.TestCase):
@@ -44,20 +44,16 @@ class TestLogSetUp(unittest.TestCase):
 		self.pars = ParserLog(log)
 
 
-	def test_count_send_to_1(self):
-		self.assertEqual(self.pars.count_send_to(), 3)
-
-	def test_count_send_to_2(self):
-		self.assertNotEqual(self.pars.count_send_to(), 2)
-
-	def test_count_send_to_3(self):
-		self.assertNotEqual(self.pars.count_send_to(), True)
-
 	def test_count_success_send_1(self):
 		self.assertNotEqual(self.pars.count_success_send(), 99 )
 
 	def test_count_success_send_2(self):
 		self.assertEqual(self.pars.count_success_send(), 2 )
+
+	def test_count_success_send_3(self):
+		log = ''
+		pars = ParserLog(log)
+		self.assertEqual(pars.count_success_send(), 0 )
 
 	def test_count_falled_send_1(self):
 		self.assertEqual(self.pars.count_falled_send(), 1 )
@@ -65,9 +61,10 @@ class TestLogSetUp(unittest.TestCase):
 	def test_count_falled_send_2(self):
 		self.assertNotEqual(self.pars.count_falled_send(), 15 )
 
-	def test_contr_sum(self):
-		summ = self.pars.count_success_send() + self.pars.count_falled_send()
-		self.assertEqual(self.pars.count_send_to(), summ)
+	def test_count_falled_send_3(self):
+		log = ''
+		pars = ParserLog(log)
+		self.assertEqual(pars.count_falled_send(), 0 )
 
 
 
